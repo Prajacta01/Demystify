634cc15f48c0f58b75d62ae261ea6549fc1848d8
isita <isita.bagayatkar@gmail.com>
2023-06-25 12:22:48 -0700

    can scrape, but not the right contents?

--- popupScript.js
+++ popupScript.js
@@ -1,11 +1,24 @@
-chrome.extension.getBackgroundPage().console.log('foo');
-// chrome.action.onClicked.addListener(function(tab) {
-//   chrome.scripting.executeScript({
-//     target: { tabId: tab.id },
-//     function: () => document.documentElement.outerHTML
-//   }, function(results) {
-//     const pageContents = results[0].result;
-//     console.log(pageContents);
-//     // Process or utilize the page contents as needed
-//   });
-// });
+let scrapePage = document.getElementById('scrapePage')
+
+// receive content from content script
+chrome.runtime.onMessage.addListener((request, sender, sendresponse) => {
+  let content = request.content
+  alert(content)
+})
+
+scrapePage.addEventListener('click', async () => {
+  let [tab] = await chrome.tabs.query({active: true, currentWindow:true});
+
+  chrome.scripting.executeScript({
+    target: {tabId: tab.id},
+    func: scrapePageContents,
+  })
+})
+
+function scrapePageContents() {
+  
+
+  let content = document.body.innerHTML
+
+  chrome.runtime.sendMessage({content})
+}
